242b2fbfe2220a5ab1428f95b8a3ce72a6342b88
cpmoore0317 <cpmoore317@gmail.com>
2024-09-20 15:29:46 -0500

    Create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <form>
+        <input type="text" placeholder="New List Name" />
+        <button type="submit">Add List</button>
+      </form>
+    </footer>
+  );
+};
+
+export default Footer;
