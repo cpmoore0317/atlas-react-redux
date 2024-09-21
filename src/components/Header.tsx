250cb02cd5671d5932e108ffafbf7232da555e5e
cpmoore0317 <cpmoore317@gmail.com>
2024-09-21 15:38:22 -0500

    Update header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
-    <header>
-      <img src="../mockup/logo.png" alt="Logo" />
+    <header className="sticky top-0 border-b-2 border-blue bg-off-white-light pb-8 pt-8">
+      <img className="logo mx-auto w-56" src="../mockup/logo.png" alt="logo" />
     </header>
   );
 };
