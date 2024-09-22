be8126cca4cae6bcb965fd0a3e3e296a966cfb42
cpmoore0317 <cpmoore317@gmail.com>
2024-09-22 15:38:17 -0500

    Updated App to incorporate all components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import Header from './Header';
-import Footer from './Footer';
 import Board from './Board';
+import Footer from './Footer';
 
 function App() {
   return (
