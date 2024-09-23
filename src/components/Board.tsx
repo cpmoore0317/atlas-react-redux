0d0c0f3e871e29f9ef098f03902f9ab241e00fc0
cpmoore0317 <cpmoore317@gmail.com>
2024-09-23 16:46:50 -0500

    Card deletion functionality

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,28 @@
+// src/components/Board.tsx
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
 import List from './List';
+import Card from './Card';
 
 const Board: React.FC = () => {
   const lists = useSelector((state: RootState) => state.lists.lists);
+  const cards = useSelector((state: RootState) => state.cards.cards);
 
   return (
     <div className="m-auto h-screen w-screen overflow-x-scroll text-center">
       <div className="flex h-full space-x-4">
         {lists.map((list) => (
-          <List key={list.id} title={list.title} listId={list.id} />
+          <div key={list.id}>
+            <List title={list.title} listId={list.id} />
+            <div className="cards-container">
+              {cards
+                .filter((card) => card.listId === list.id) // Filter cards by list ID
+                .map((card) => (
+                  <Card key={card.id} id={card.id} title={card.title} description={card.description} />
+                ))}
+            </div>
+          </div>
         ))}
       </div>
     </div>
