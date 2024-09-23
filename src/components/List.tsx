306634fe879ca698dd49787a628678fba62a1e37
cpmoore0317 <cpmoore317@gmail.com>
2024-09-23 14:15:18 -0500

    List update

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteList } from '../slices/listsSlice';
 import DeleteListButton from './DeleteListButton';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
 
-interface CardData {
-  title: string;
-  description: string;
-}
-
 interface ListProps {
   title: string;
-  cards: CardData[];
+  listId: string;
 }
 
-const List: React.FC<ListProps> = ({ title, cards }) => {
+const List: React.FC<ListProps> = ({ title, listId }) => {
+  const dispatch = useDispatch();
+
+  const handleDeleteList = () => {
+    dispatch(deleteList(listId));
+  };
+
   return (
     <div className="group/list h-full min-w-96 p-4">
-      <DeleteListButton />
+      <DeleteListButton onClick={handleDeleteList} />
       <h3>{title}</h3>
-      {cards.map((card, index) => (
-        <Card key={index} title={card.title} description={card.description} />
-      ))}
-      <NewCardForm />
+      {/* Add Card components here */}
+      <NewCardForm listId={listId} />
     </div>
   );
 };
